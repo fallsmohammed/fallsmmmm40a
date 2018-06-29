03aa647dff1db5a3aa1cae89a02e1f7b1f5f813e
falls86 <40698133+falls86@users.noreply.github.com>
2018-06-29 20:16:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,4 +28,4 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
 });
 
-+ عميل . تسجيل الدخول ( NDU5ODY0MDA0ODA3MDk4Mzc4Dhf0BgD6Y5BEJIP6IX6germ8Vt0xkPpLA
+client.login(process.env.BOT_TOKEN);
