7306f3dd7a5883b23b63b62abf0a806f27f0a401
falls86 <40698133+falls86@users.noreply.github.com>
2018-07-01 00:10:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,5 +27,26 @@ client.user.setGame(`Ventuno`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+        client.on('message', message => {
+                        let args = message.content.split(" ").slice(1).join(" ")
+if(message.content.startsWith(prefix + 'color')) {
+    if(!args) return message.channel.send('`يرجي اختيار كم لون `');
+             if (!message.member.hasPermission('MANAGE_ROLES')) return message.channel.sendMessage('`**⚠ | `[MANAGE_ROLES]` لا يوجد لديك صلاحية**'); 
+             message.channel.send("**✅ | تم عمل الالوان**");
+                  setInterval(function(){})
+                    let count = 0;
+                    let ecount = 0;
+          for(let x = 1; x < `${parseInt(args)+1}`; x++){
+            message.guild.createRole({name:x,
+              color: 'RANDOM'})
+              }
+            }
+       });
+
+    
+
+ 
+
+
 
 client.login(process.env.BOT_TOKEN);
