465ef2e9ed01133a5ef7e92c1be25ed7430ab2a6
falls86 <40698133+falls86@users.noreply.github.com>
2018-07-01 21:58:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,35 +27,6 @@ client.user.setGame(`Ventuno`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-+
-  client.on('message', message => {
-  if (true) {
-if (message.content === 'invite') {
-      message.author.send('رابط البوت').catch(e => console.log(e.stack));
-
-    }
-   } 
-  });
-+
-
-client.on('message', message => {
-     if (message.content === "invite") {
-     let embed = new Discord.RichEmbed()
-  .setAuthor(message.author.username)
-  .setColor("#9B59B6")
-  .addField(" Done | تــــم" , " |  تــــم ارســالك في الخــاص")
-     
-     
-     
-  message.channel.sendEmbed(embed);
-    }
-});
-+
-
-https : //hastebin.com/qofekanapa.coffeescript
-+
-
-
 
 
 
